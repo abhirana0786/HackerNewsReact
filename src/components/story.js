ad6217a56f69dc7029961c21c2b62393a3c917e2
Abhinav Rana <abhi.rana0786@gmail.com>
2020-05-09 23:00:26 +0530

    local storage UpVote Count

--- src/components/story.js
+++ src/components/story.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 import React, { useState, useEffect } from 'react';
 import './story.css';
 import { getStoryData } from '../services/hnApi';
@@ -13,6 +14,7 @@ const StoryComponent = () => {
 
     const [storyData, setStoryData] = useState([]);
     const [currentPage, setCurrentPage] = useState(0);
+    const [upVote, setUpVote] = useState({});
     useEffect(() => {
         getStoryData({ page: 0 }).then(data => setStoryData(data));
     }, []);
@@ -20,6 +22,8 @@ const StoryComponent = () => {
     useEffect(() => {
         getStoryData({ page: currentPage }).then(data => setStoryData(data));
     }, [currentPage]);
+
+
 
     //const maxPage = Math.ceil(storyData.hits.length / hitsPerPage);
     const prevPage = () => {
@@ -35,10 +39,21 @@ const StoryComponent = () => {
     }
     const addCount = (obj) => {
         let masterData = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : [];
-        masterData.push({ id: obj.objectID, points: obj.points });
+        let oldData = masterData[masterData.map((val) => { return val.id }).indexOf(obj.objectID)];
+        masterData = masterData.filter((val) => { return val.id !== obj.objectID });
+        let dataVal = {};
+        if (oldData && oldData.id) {
+            dataVal = { id: obj.objectID, points: (oldData.points + 1) };
+
+        } else {
+            dataVal = { id: obj.objectID, points: (obj.points + 1) };;
+        }
+        setUpVote(dataVal);
+        masterData.push(dataVal);
+
         localStorage.setItem('data', JSON.stringify(masterData));
     }
-    console.log('currentPage', currentPage)
+
 
     return (
         <div className="container">
@@ -61,7 +76,7 @@ const StoryComponent = () => {
                     {storyData && storyData.hits && storyData.hits.map((data, index) => {
                         return <tr key={index}>
                             <td>{data.num_comments}</td>
-                            <td>{data.points}</td>
+                            <td> {upVote && upVote.id && upVote.id === data.objectID ? upVote.points : data.points}</td>
                             <td><img onClick={() => addCount(data)} className="counticon pointer" src="/arrow.svg" alt="" /></td>
                             <td><a href="" className="storylink">{data.title}</a> <span className="siteurl">({data.url})</span> <span>by</span> <a href={`user?id={data.author}`} className="hnuser">{data.author}</a> <span className="time">{moment(data.created_at).fromNow()}</span> <span>[<a href="" className="hidebtn"> Hide </a>]</span></td>
                         </tr>
