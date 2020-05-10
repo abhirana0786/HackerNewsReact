273c5a68dfe27ac00e68073813b8511f43059db6
Abhinav Rana <abhi.rana0786@gmail.com>
2020-05-10 22:44:56 +0530

    Hide Story and Update VoteCount in Graph

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,41 +1,45 @@
-import React, { Component } from 'react';
+/* eslint-disable array-callback-return */
+import React from 'react';
 import CanvasJSReact from '../assets/canvasjs.react';
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-class SplineChart extends Component {
-    render() {
-        const options = {
-            animationEnabled: true,
-            axisX: {
-                title: "ID"
-            },
-            axisY: {
-                title: "Votes",
-                includeZero: true,
-                interval: 250
-            },
-            data: [{
-                type: "spline",
-                dataPoints: [
-                    { label: 213, y: 500 },
-                    { label: 324, y: 550 },
-                    { label: 624, y: 750 },
-                    { label: 57, y: 600 },
-                    { label: 87, y: 800 },
-                    { label: 1, y: 750 }
-                ]
-            }]
-        }
+const SplineChart = (props) => {
 
-        return (
-            <div>
-                <CanvasJSChart options={options}
-                /* onRef={ref => this.chart = ref} */
-                />
-                {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-            </div>
-        );
+
+    const chartData = [];
+    props.data && props.data.hits && props.data.hits.map((data, index) => {
+        let masterData = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : [];
+        let oldData = masterData[masterData.map((val) => { return val.id }).indexOf(data.objectID)];
+        // hide record
+        let hideMasterData = localStorage.getItem('hide-data') ? JSON.parse(localStorage.getItem('hide-data')) : [];
+        let hideOldData = hideMasterData[hideMasterData.map((val) => { return val }).indexOf(data.objectID)];
+        if (!hideOldData) {
+            chartData.push({ label: data.objectID, y: (oldData && oldData.points) ? oldData.points : data.points });
+        }
+    });
+    const options = {
+        animationEnabled: true,
+        axisX: {
+            title: "ID"
+        },
+        axisY: {
+            title: "Votes",
+            includeZero: true
+        },
+        data: [{
+            type: "spline",
+            dataPoints: chartData
+        }]
     }
+
+    return (
+        <div>
+            {chartData && (<CanvasJSChart options={options}
+            /* onRef={ref => this.chart = ref} */
+            />)}
+        </div>
+    );
+
 }
 
 export default SplineChart;
--- src/components/story.js
+++ src/components/story.js
@@ -1,3 +1,5 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 // eslint-disable-next-line
 import React, { useState, useEffect } from 'react';
 import './story.css';
@@ -7,7 +9,6 @@ import SplineChart from './chart';
 
 const StoryComponent = () => {
     // const [count, setCount] = useState(0);
-
     // const countUp = () => {
     //     setCount(count + 1);
     //     console.log(count);
@@ -24,9 +25,6 @@ const StoryComponent = () => {
         getStoryData({ page: currentPage }).then(data => setStoryData(data));
     }, [currentPage]);
 
-
-
-    //const maxPage = Math.ceil(storyData.hits.length / hitsPerPage);
     const prevPage = () => {
         if (currentPage > 0) {
             setCurrentPage(currentPage - 1);
@@ -38,6 +36,7 @@ const StoryComponent = () => {
             setCurrentPage(currentPage + 1);
         }
     }
+
     const addCount = (obj) => {
         let masterData = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : [];
         let oldData = masterData[masterData.map((val) => { return val.id }).indexOf(obj.objectID)];
@@ -56,8 +55,12 @@ const StoryComponent = () => {
     }
 
     const hideStory = (id) => {
+        let masterData = localStorage.getItem('hide-data') ? JSON.parse(localStorage.getItem('hide-data')) : [];
+        masterData = masterData.filter((val) => { return val.id !== id });
+        masterData.push(id);
+        localStorage.setItem('hide-data', JSON.stringify(masterData));
         const updatedData = storyData.hits.filter((item) => { return item.objectID !== id });
-        return updatedData;
+        setStoryData({ hits: updatedData });
     }
 
     return (
@@ -72,29 +75,34 @@ const StoryComponent = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {/* <tr>
-                        <td>36</td>
-                        <td>{count}</td>
-                        <td onClick={countUp}>up</td>
-                        <td>Lorem Ispsums</td>
-                    </tr> */}
                     {storyData && storyData.hits && storyData.hits.map((data, index) => {
-                        let domain = data.url.split("//")[1];
-                        let mainurl = domain.split("/")[0];
+                        // show vote count
+                        let masterData = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : [];
+                        let oldData = masterData[masterData.map((val) => { return val.id }).indexOf(data.objectID)];
+                        // hide record
+                        let hideMasterData = localStorage.getItem('hide-data') ? JSON.parse(localStorage.getItem('hide-data')) : [];
+                        let hideOldData = hideMasterData[hideMasterData.map((val) => { return val }).indexOf(data.objectID)];
 
-                        return <tr key={index}>
-                            <td>{data.num_comments}</td>
-                            <td> {upVote && upVote.id && upVote.id === data.objectID ? upVote.points : data.points}</td>
-                            <td><img onClick={() => addCount(data)} className="counticon pointer" src="/arrow.svg" alt="" /></td>
-                            <td><a href={data.url} className="storylink">{data.title}</a> <span className="siteurl">({mainurl})</span> <span>by</span> <a href={`user?id=${data.author}`} className="hnuser">{data.author}</a> <span className="time">{moment(data.created_at).fromNow()}</span> <span>[<a onClick={() => hideStory(data.objectID)} className="hidebtn pointer"> Hide </a>]</span></td>
-                        </tr>
+                        let domain = data && data.url && data.url.split("//")[1];
+                        let mainurl = domain && domain.split("/")[0];
+                        console.log(hideOldData);
+                        if (!hideOldData) {
+
+
+                            return <tr key={index}>
+                                <td>{data.num_comments}</td>
+                                <td> {upVote && upVote.id && upVote.id === data.objectID ? upVote.points : (oldData && oldData.points) ? oldData.points : data.points}</td>
+                                <td><img onClick={() => addCount(data)} className="counticon pointer" src="/arrow.svg" alt="" /></td>
+                                <td><a href={data.url} className="storylink">{data.title}</a> <span className="siteurl">({mainurl})</span> <span>by</span> <a href={`user?id=${data.author}`} className="hnuser">{data.author}</a> <span className="time">{moment(data.created_at).fromNow()}</span> <span>[<a onClick={() => hideStory(data.objectID)} className="hidebtn pointer"> Hide </a>]</span></td>
+                            </tr>
+                        }
 
                     })}
                 </tbody>
             </table>
             <div className="pagination"><a className="pointer" onClick={prevPage}>Previous</a> | <a className="pointer" onClick={nextPage}>Next</a></div>
             <div className="chart-view">
-                <SplineChart />
+                <SplineChart data={storyData} />
             </div>
         </div>
     )
